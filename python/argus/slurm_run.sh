185742dfb22e8fe70e4184bce50ccbd2d4cc8381
Tom Kimpson <tomkimpson@gmail.com>
2025-06-02 12:52:02 +1000

    sw

--- python/argus/slurm_run.sh
+++ python/argus/slurm_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
  
 #SBATCH --job-name=nested_sampling_test
-#SBATCH --output=outputs/logfiles/nested_sampling_test_output5.txt
+#SBATCH --output=outputs/logfiles/nested_sampling_test_output8.txt
 #SBATCH --export=ALL 
 #SBATCH --gres=gpu:1
-#SBATCH --time=12:00:00 
+#SBATCH --time=72:00:00 
 #SBATCH --mem=8G
 
 
