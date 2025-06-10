310cb688d8a89048433228969d7c18cb45b133e4
Tom Kimpson <tomkimpson@gmail.com>
2025-06-10 16:36:53 +1000

    add run in serial

--- python/argus/slurm_run.sh
+++ python/argus/slurm_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
  
-#SBATCH --job-name=numpyro_test_010
-#SBATCH --output=outputs/logfiles/numpyro_test_010_output.txt
+#SBATCH --job-name=numpyro_test_011
+#SBATCH --output=outputs/logfiles/numpyro_test_011_output.txt
 #SBATCH --export=ALL 
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --time=2:00:15 
 #SBATCH --mem=8G
 
 
 source ~/.bashrc
 conda activate Argus
-time python -u main.py configs/config_numpyro_test_010.ini
+time python -u main.py configs/config_numpyro_test_011.ini
