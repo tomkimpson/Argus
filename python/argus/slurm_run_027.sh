18660c3baf3f5a943431c1fff25abc256dbe2f50
Tom Kimpson <tomkimpson@gmail.com>
2025-07-14 15:05:49 +1000

    Enable EFAC and EQUAD as free parameters

---
+++ python/argus/slurm_run_027.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+ 
+#SBATCH --job-name=numpyro_test_027
+#SBATCH --output=outputs/logfiles/numpyro_test_027_output.txt
+#SBATCH --export=ALL 
+#SBATCH --gres=gpu:4
+#SBATCH --time=12:00:00 
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=8
+
+source ~/.bashrc
+conda activate Argus
+time python -u main.py configs/config_numpyro_test_027.ini
